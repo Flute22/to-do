a514dab201b5d0994ba30bd476ddecb01659bef2
khushalsinhmar <shimarsabb@gmail.com>
2025-05-16 13:03:31 +0530

    add utility classes for API error handling and response management

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+export const asyncHandler = ( requestHandler ) => {
+    return ( req, res, next ) => {
+        Promise
+            .resolve( requestHandler(req, res, next) )
+            .catch((err) => {
+                res.status(err.statusCode || 500).json({
+                    success: false,
+                    message: err.message,
+                    errors: err.error || []
+                })
+            })
+    }
+}
